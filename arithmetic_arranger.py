cd29fbd46f8e8f1481f34ac5872768679848110e
Roberto0611 <129466817+Roberto0611@users.noreply.github.com>
2023-12-04 18:38:15 +0000

    Check for errors in the format

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,41 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, answer_):
 
+  #Variables
+  arranged_problems = []
+  input = problems
+  operations = []
+  results = []
 
-    return arranged_problems
+  #Loops to get a list with a list of the operations
+  for element in input:
+    operations.append(element.split())
+
+  #Check for any error
+
+  #First Rule of errors
+  NumberOfOperations = len(operations)
+  if NumberOfOperations > 5:
+    return 'Error: Too many problems.'
+
+  #Get nums individually
+  for operation in operations:
+    try:
+      num1 = int(operation[0])
+      sign = operation[1]
+      num2 = int(operation[2])
+
+      #Check if signs are correct
+      if sign not in ['+', '-']:
+        return "Error: Operator must be '+' or '-'."
+
+      #Check if numbers are correct
+      if num1 > 999 or num2 > 999:
+        return "Error: Numbers cannot be more than four digits."
+
+      print(num1, sign, num2)
+
+    #Check if there's only digits
+    except ValueError:
+      return 'Error: Numbers must only contain digits.'
+
+  return operations
